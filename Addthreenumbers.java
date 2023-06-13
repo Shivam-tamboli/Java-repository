368c883909e9b45a529b8713ca9981fdb8f6c810
shivam Tamboli <shivamtamboli66@gmail.com>
2023-06-13 19:19:46 +0530

    The sum of three numbers

---
+++ Addthreenumbers.java
@@ -0,0 +1,21 @@
+public class Addthreenumbers {
+    public static void main(String[] args) {
+        // intiger number1 value.
+        int num1 = 12;
+        // intiger number2 value.
+        int num2 = 12;
+        // intiger number3 value
+        int num3 = 12;
+        // sum of all intiger numbers in Sum
+        int Sum = num1+num2+num3;
+        // A descriptive message before printing sum
+        System.out.println("The sum of three number is:");
+        // Printing sum
+        System.out.println(Sum);
+    }
+    
+}
+
+// Expected output:
+// The sum of three number is:
+// 36  
