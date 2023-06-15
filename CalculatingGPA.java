d689cc25584830f3454d6a2f78d66269d878f599
shivam Tamboli <shivamtamboli66@gmail.com>
2023-06-15 20:49:21 +0530

    Taking input from the user of his marks of every subject and calaulating the GPA

---
+++ CalculatingGPA.java
@@ -0,0 +1,24 @@
+
+public class CalculatingGPA {
+    public static void main(String[] args) {
+        // Statement you want to print.
+        System.out.println("Your CGPA is :");
+        // subject 1 marks.
+        float subject1 = 75;
+        // Subject 2 marks.
+        float subject2 = 95;
+        // Subject 3 marks.
+        float subject3 = 89;
+        // Calculating total and divided by thirty.
+        float CGPA = (subject1 + subject2 + subject3)/30;
+        // Printing CGPA.
+        System.out.println(CGPA); 
+    
+    }
+}
+
+// Expected output is.
+// Your CGPA is :
+// 8.633333
+
+
