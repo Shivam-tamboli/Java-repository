8de361a624559f5df0d0104265bd9e5ea111dc52
shivam Tamboli <shivamtamboli66@gmail.com>
2023-06-24 18:04:22 +0530

    Printing stars in half pyramid pattern

---
+++ printingpattern.java
@@ -0,0 +1,19 @@
+public class printingpattern {
+    public static void main(String[] args) {
+        // Printing pattern in with star in half paramid.
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+    }
+    
+}
+
+
+// EXpected output is.
+// *
+// **
+// ***
+// ****
+// *****
