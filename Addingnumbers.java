4c401cb9405f52e364b11e9dbe522d609e50027e
shivam Tamboli <shivamtamboli66@gmail.com>
2023-06-24 18:02:51 +0530

    Adding two numbers and printing the sum

---
+++ Addingnumbers.java
@@ -0,0 +1,15 @@
+public class Addingnumbers {
+    public static void main(String[] args) {
+        // created a integer, name is a and stored value in intiger.
+        int a = 12;
+        // created a integer, name is b and stored value in intiger.
+        int b = 12;
+        // created a integer name of sum will add a and b, it will store the total of a and b.
+        int sum = a + b ;
+        // Printing the sum.
+        System.out.println(sum);
+    }
+    
+}
+// Expected output.
+// 24
