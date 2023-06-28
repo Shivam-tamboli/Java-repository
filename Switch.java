a8a3dd6ac3959b7db7397c20ee2da0e7e33cc772
shivam Tamboli <shivamtamboli66@gmail.com>
2023-06-28 18:49:44 +0530

    using switch statement for multiple cases taking input from the user and using switch statement

---
+++ Switch.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Switch {
+    public static void main(String[] args) {
+        System.out.println("Enter a number.");
+        Scanner sc = new Scanner(System.in);
+        // Taking input from the user.
+        int button = sc.nextInt();
+//      If we have multiple cases the we wil use switch statement.
+        switch(button){
+            // Case 1, if user gave input 1.
+            case 1: System.out.println("Hello.");
+            // break.
+            break;
+
+            // case 2, if user gave input 2.
+            case 2: System.out.println("Namaste.");
+            // break.
+            break;
+
+            // case 3, if user gave input 3.
+            case 3: System.out.println("Bonjure.");
+            // break.
+            break;
+
+            // Using default if the user gave different input .
+            default: System.out.println("Invalid option."); 
+
+
+
+        }
+    }
+    
+}
