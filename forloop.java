dc08426f62ac854416037fad026b30e5c12f5d4a
shivam Tamboli <shivamtamboli66@gmail.com>
2023-06-30 23:29:33 +0530

    Using for loop for printing a string in a loop

---
+++ forloop.java
@@ -0,0 +1,21 @@
+public class forloop {
+    public static void main(String[] args)
+    // Using for loop for printing shivam.
+    {
+        // first initialization, second condition,third updation
+        for(int counter = 0; counter < 10; counter = counter + 1){
+            System.out.println("Shivam");
+        }
+    }
+}
+// Expected output.
+// Shivam
+// Shivam
+// Shivam
+// Shivam
+// Shivam
+// Shivam
+// Shivam
+// Shivam
+// Shivam
+// Shivam
