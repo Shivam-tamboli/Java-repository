640b1b2a3e0887a07342c5d20879a288ca22e5df
shivam Tamboli <shivamtamboli66@gmail.com>
2023-07-01 20:51:00 +0530

    Printing the numbers form 0 to 10 with the help of while loop

---
+++ whileloop.java
@@ -0,0 +1,28 @@
+public class whileloop {
+    public static void main(String[] args) {
+        // Initializing .
+        int counter = 0;
+        // Condition.
+        // It will print 0 to 18.
+        while(counter <19) {
+            System.out.println(counter);
+            // Updation
+            counter++;
+        }
+    }
+    
+}
+
+// Expected output.
+// 0
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
+// 11
