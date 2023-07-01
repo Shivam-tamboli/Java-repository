ff11b9b6e417b20d5856ea3b36f2596cb2238393
shivam Tamboli <shivamtamboli66@gmail.com>
2023-07-01 20:53:14 +0530

    in do while loop printed numbers and took false condition for checking but it got printed one time

---
+++ Dowhileloop.java
@@ -0,0 +1,43 @@
+public class Dowhileloop {
+    public static void main(String[] args) {
+        // Initilizing .
+        int counter = 60;
+        // do{
+        //     System.out.println(counter);
+        //     counter++;
+        // } while(counter < 12);
+
+        // Taking false condition for dowhile loop.
+        // Using do whileloop.
+            do{
+                System.out.println("Shivam");
+                // Updation
+                counter++;
+
+                // Condition.
+            }while(counter < 21);
+
+// `               It does not matter condition is true or false, but do while will print one time.
+
+
+
+    }
+    
+}
+
+// Expected output.
+// 0
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
+// 11
+
+// Expected output for false condition, // It worked.
+// Shivam
