2c3242011e5546d5a7536642d428ae6758ee1b91
shivam Tamboli <shivamtamboli66@gmail.com>
2023-07-01 19:40:39 +0530

    Using for loop and printing 0 to 10 numbers

---
+++ Foorloop.java
@@ -0,0 +1,10 @@
+public class Foorloop {
+    public static void main(String[] args) {
+        // Using for loop printed 0 to 10 numbers.
+        for (int counter = 0; counter < 11; counter = counter + 1 ){
+            System.out.println(counter+" ");
+        }
+
+    }
+    
+}
