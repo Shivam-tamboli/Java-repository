18090b01b59d905fdc1e2a1697e636ab9ba757e9
shivam Tamboli <shivamtamboli66@gmail.com>
2023-07-03 23:13:32 +0530

    Printed java in a java partten

---
+++ Javapattern.java
@@ -0,0 +1,17 @@
+public class Javapattern {
+    public static void main(String[] args) {
+        System.out.println("      j         A           v           v          A             ");
+        System.out.println("      j       A    A          v       v          A    A           ");
+        System.out.println(" j    j     A   A A  A          v   v          A  A   A  A        ");
+        System.out.println("   j  j   A            A          v          A             A     ");
+        System.out.println("                                                                ");
+
+    }
+    
+}
+
+// Expected output.
+//       j         A           v           v          A
+//       j       A    A          v       v          A    A
+//  j    j     A   A A  A          v   v          A  A   A  A
+//    j  j   A            A          v          A             A
