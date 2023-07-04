69a54c4acc96dfbe564c18acdd379769be0a4144
shivam Tamboli <shivamtamboli66@gmail.com>
2023-07-04 22:18:38 +0530

    Using for loops and nested loops for printing the solid rectangle with stars

---
+++ solidrectangle.java
@@ -0,0 +1,27 @@
+public class solidrectangle {
+    public static void main(String[] args) {
+        // Intiger value.
+        int a = 4;
+        // Intiger value.
+        int b = 5;
+        // Using for loops.
+        // Nested loops.
+        // Prinitng the stars in a solid rectangle.
+        for(int counter = 1; counter < a; counter++){
+                for(int i = 1; i < b; i++){
+                    System.out.print("*");
+
+
+            }
+            // For printing continue for next line.
+            System.out.println();
+
+        }
+    }
+    
+}
+
+// Expected output.
+// ****
+// ****
+// ****
