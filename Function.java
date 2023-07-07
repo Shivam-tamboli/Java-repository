feb4b300b42b1d6aa9f7dc0584165dfb97ef2c3f
shivam Tamboli <shivamtamboli66@gmail.com>
2023-07-07 20:44:12 +0530

    Created a function and taking arguments form the user and printing

---
+++ Function.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+// Created a function name by printmyname.
+public class Function {               
+    //            String type argument, agrument is name.
+    public static void printmyname (String name){
+        System.out.println(name);
+        return;
+//      return.
+    }
+    public static void main(String[] args) {
+        // Created a scanner object.
+        Scanner sc = new Scanner(System.in);
+        // Taking input from the user as a string.
+        String name = sc.next();
+//         Calling the function and giving the argunment.
+        printmyname(name);
+    }
+    
+}
+// Expected output is .
+// Taking input from the user as a string.
+// Shivam
+// Shivam
