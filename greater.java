dd87d1d5b5f79077e3f09f69bcd763db66eece2c
shivam Tamboli <shivamtamboli66@gmail.com>
2023-07-13 22:52:52 +0530

    created a function and it takes input and give the greater number

---
+++ greater.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class greater {
+    // Creater a function taking argunment.
+    public static int Namefunction(int a,int b){
+        // Using if condition statement.
+        if(a>b){
+            // it returns a.
+            return a;
+    }
+    else {
+        // Return b.
+        return b;
+
+    }
+        
+    }
+
+    public static void main(String[] args) {
+        // Created a scanner class object.
+        Scanner sc = new Scanner (System.in);
+        // Printing statement for a.
+        System.out.println("Enter the number");
+        // Taking input form the user.
+        int a = sc.nextInt();
+        // Printing statement for b.
+        System.out.println("Enter the number");
+        // Taking input form the  user.
+        int b = sc.nextInt();
+        // Printing statement.
+        System.out.println("The greater number is");
+        // Calling function.
+        System.out.println(Namefunction(a,b));
+
+
+    }
+    
+}
+// Expected output is.
+// Enter the number
+// 65
+// Enter the number
+// 56
+// The greater number is
+// 65
