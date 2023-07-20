abd1fcab067c6d29f1b942faf9484728a8f5e833
shivam Tamboli <shivamtamboli66@gmail.com>
2023-07-20 23:11:50 +0530

    Created an array and using loop for printing an array

---
+++ Array.java
@@ -0,0 +1,20 @@
+// Created an array.
+public class Array {
+    public static void main(String[] args) {
+        //
+       int [] marks = new int[3];
+       marks[0] = 89;
+       marks[1] = 98;
+       marks[2] = 99;
+    //    System.out.println(marks[1]);
+    // For prininting an array using loop.
+       for(int i = 0; i<3; i++){
+        System.out.println(marks[i]);
+       }
+    }
+    
+}
+// Expected output is.
+// 89
+// 98
+// 99
