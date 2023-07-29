b5d0bdf92501ab062d5e3707848d30af3f8e3d2c
shivam Tamboli <shivamtamboli66@gmail.com>
2023-07-29 18:37:14 +0530

    Created an array

---
+++ Array4.java
@@ -0,0 +1,15 @@
+public class Array4 {
+    public static void main(String[] args) {
+        
+    // Declaring an array, array name is marks.
+    int [] marks = new int[3];
+       marks[0] = 89;
+       marks[1] = 98;
+       marks[2] = 99;
+    // For prininting an array using loop.
+       for(int i = 0; i<3; i++){
+        System.out.println(marks[i]);
+       }
+    
+}
+}
