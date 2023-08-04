7767e136bfb784ea266eb8fe461a74f325220d1c
shivam Tamboli <shivamtamboli66@gmail.com>
2023-08-04 16:56:15 +0530

    Created a string bulider inserting and set the a string in a string bulider

---
+++ Stringbuilder.java
@@ -0,0 +1,26 @@
+public class Stringbuilder {
+public static void main(String[] args) {
+    // Crerated a string bulider,and stored string.
+    StringBuilder sb = new StringBuilder("Tony");
+    // Printing the string.
+    System.out.println(sb);
+
+    // Char  at index 0.
+    System.out.println(sb.charAt(0));
+
+    // set char at index.
+    sb.setCharAt(0,'g');
+    System.out.println(sb);
+
+    // inserting in the index.
+    sb.insert(0, 's');
+    System.out.println(sb);
+}
+    
+}
+
+// Expected output is.
+// Tony
+// T
+// gony
+// sgony
