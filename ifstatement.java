0cd7511f06a60a28b17aafc68acd583f0bdd44dc
shivam Tamboli <shivamtamboli66@gmail.com>
2023-08-11 23:39:21 +0530

    It checks number is positive or negative

---
+++ ifstatement.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class ifstatement {
+    
+
+    
+  public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input number: ");
+        int input = in.nextInt();
+
+        if (input > 0)
+        {
+            System.out.println("Number is positive");
+        }
+        else if (input < 0)
+        {
+            System.out.println("Number is negative");
+        }
+        else
+        {
+            System.out.println("Number is zero");
+        }
+    }
+}
+
+    
+
+// Expected output is.
+// Input number: 456
+// Number is positive
