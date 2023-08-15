0b2f229647be2e38ac3ab571cdd1c11c83a1f994
shivam Tamboli <shivamtamboli66@gmail.com>
2023-08-15 23:45:34 +0530

    Created a 2D array

---
+++ Array2D.java
@@ -0,0 +1,54 @@
+import java.util.*;
+public class Array2D {
+    public static void main(String args[]) {
+     // Created a scanner class instance.
+        Scanner sc = new Scanner(System.in);
+     //    Taking input from the user as an integer for rows.
+        int rows = sc.nextInt();
+        //    Taking input from the user as an integer for coloums.
+        int cols = sc.nextInt();
+ 
+ //      Declared an array.
+        int[][] numbers = new int[rows][cols];
+ 
+ 
+        //input
+     //    Using loop for the rows.
+        for(int i=0; i<rows; i++) {
+            //Using loop for the columns
+            for(int j=0; j<cols; j++) {
+                numbers[i][j] = sc.nextInt();
+            }
+        }
+ 
+ //    Taking input for the user for finding x.
+        int x = sc.nextInt();
+ 
+ //      Using loop for the rows.
+        for(int i=0; i<rows; i++) {
+         // Using loop for the rows.
+            for(int j=0; j<cols; j++) {
+                //compare with x
+                if(numbers[i][j] == x) {
+                    System.out.println("x found at location (" + i + ", " + j + ")");
+                }
+            }
+        }
+ 
+ 
+      
+ 
+ 
+    }
+ }
+ 
+ // Expected output is.
+ // 3
+ // 3
+ // 1 2 3 
+ // 4 2 1
+ // 1 2 3
+ // 4
+ // x found at location (1, 0)
+    
+
