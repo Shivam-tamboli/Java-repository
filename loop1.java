51fcedac2c73da8c274c2d8b741d60089d16bff5
shivam Tamboli <shivamtamboli66@gmail.com>
2023-09-15 16:10:25 +0530

    Using if else for checking the which condition is true

---
+++ loop1.java
@@ -0,0 +1,22 @@
+public class loop1 {
+    public static void main(String[] args) {
+        // Defined the salary.
+         int salary = 1123445;
+        //  Now using if condition.
+    if(salary>1000){
+        // if the statement is true,this will be the new salary.
+        salary = salary + 2000;
+    }
+    else{
+        // else this will be the new salary.
+        salary = salary + 3000;
+    }
+    // printing the salary.
+            System.out.println(salary);        
+
+    }
+   
+
+}
+// Expected output is.
+// 1125445
