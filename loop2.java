19090c6003e64066d990a1537da12c07d2cdede0
shivam Tamboli <shivamtamboli66@gmail.com>
2023-10-21 14:55:46 +0530

    Printing the numbers from 1 to 6 with the help of for loop

---
+++ loop2.java
@@ -0,0 +1,19 @@
+public class loop2 {
+    public static void main(String[] args) {
+        // Q = print the numbers from 1 to 6.
+    // Loops
+    // initialisation, condition ,increment.
+    for(int num = 1; num <= 6; num += 1){
+        // Printing the number.
+        System.out.println(num);
+    }
+    }
+    
+}
+// Expected output is .
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
