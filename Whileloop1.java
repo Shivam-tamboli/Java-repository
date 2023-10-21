a33c8b1386c081da959eaf210253cb3debb23e20
shivam Tamboli <shivamtamboli66@gmail.com>
2023-10-21 15:46:50 +0530

    Using while loop in this for printing the numbers

---
+++ Whileloop1.java
@@ -0,0 +1,27 @@
+public class Whileloop1 {
+    public static void main(String[] args) {
+       
+        // Initilization.
+        int num = 1;
+         // Using while loop.
+        //  condition.
+        while(num <= 9){
+            // Printing the num.
+            System.out.println(num);
+            // Incrementation of num by one.
+            num++;
+        }
+    }
+    
+}
+// Expected output is.
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+
