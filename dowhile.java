eb6ce359d93c21130ed36e7d59547cd804b0e660
shivam Tamboli <shivamtamboli66@gmail.com>
2023-10-26 20:40:09 +0530

    In do while loop executing the program and checking the condition

---
+++ dowhile.java
@@ -0,0 +1,18 @@
+public class dowhile {
+    public static void main(String[] args) {
+    
+        // initialziation.
+        int n = 1;
+        // Using do while loop.
+        do{
+            // Printing.
+            System.out.println("Hello world");
+
+        } while(n != 1);
+        // Condition.
+    }
+    
+    
+}
+// Expected output is.
+// Hello world
